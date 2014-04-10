93419c59b83f9442ea9620d1239f1798cfb06822
SachinSaxena08 <sachinsaxena08@yahoo.co.in>
2014-04-10 23:02:58 +0530

    First Commit ronm Git GUI Signed-off-by: SachinSaxena08 <sachinsaxena08@yahoo.co.in>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppHeruko::Application.config.secret_token = '557c847486117ad879f1977087d53dfac4d5ca4e9057482f97ad8afa8f1f0450c60eae69f9524a33f7559890f5c35a1e12a12d08d9f90cab3f7f7644e21c7f15'
